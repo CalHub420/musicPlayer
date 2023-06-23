d07fb3a0aa72c5bb3205aebd83858a4659842112
CalHub420 <120170568+CalHub420@users.noreply.github.com>
2023-06-23 12:00:00 +0100

    MusicPlayer
    Initial Commit

---
+++ musicPlayer/index.js
@@ -0,0 +1,110 @@
+const image = document.getElementById("cover"),
+  title = document.getElementById("music-title"),
+  artist = document.getElementById("music-artist"),
+  currentTimeEl = document.getElementById("current-time"),
+  durationEl = document.getElementById("duration"),
+  progress = document.getElementById("progress"),
+  playerProgress = document.getElementById("player-progress"),
+  prevBtn = document.getElementById("prev"),
+  nextBtn = document.getElementById("next"),
+  playBtn = document.getElementById("play"),
+  background = document.getElementById("bg-img");
+
+const music = new Audio();
+
+const songs = [
+  {
+    path: "./assets/onlymp3.to - TOOL - Pneuma Audio -5ClCaPmAA7s-192k-1687271141.mp3",
+    displayName: "Pneuma",
+    cover: "./assets/cover1.jpg",
+    artist: "TOOL",
+  },
+  {
+    path: "./assets/onlymp3.to - Deftones - RX Queen-WkN0ARwbPeA-192k-1687271270.mp3",
+    displayName: "Rx-Queen",
+    cover: "./assets/cover2.jpg",
+    artist: "Deftones",
+  },
+  {
+    path: "./assets/onlymp3.to - System Of A Down - Spiders H Q -enQoWeKyVRM-192k-1687271324.mp3",
+    displayName: "Spiders",
+    cover: "./assets/cover3.jpg",
+    artist: "System Of A Down",
+  },
+];
+
+let musicIndex = 0;
+let isPlaying = false;
+
+function togglePlay() {
+  if (isPlaying) {
+    pauseMusic();
+  } else {
+    playMusic();
+  }
+}
+
+function playMusic() {
+  isPlaying = true;
+  //Change play button icon
+  playBtn.classList.replace("fa-play", "fa-pause");
+  //Set button gover title
+  playBtn.setAttribute("title", "Pause");
+  music.play();
+}
+
+function pauseMusic() {
+  isPlaying = false;
+  //Change pause button icon
+  playBtn.classList.replace("fa-pause", "fa-play");
+  //Set button gover title
+  playBtn.setAttribute("title", "Play");
+  music.pause();
+}
+
+function loadMusic(song) {
+  music.src = song.path;
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  image.src = song.cover;
+  background.src = song.cover;
+
+  music.addEventListener("loadedmetadata", () => {
+    const duration = music.duration;
+    const formattedDuration =
+      formatTime(duration / 60) + ":" + formatTime(duration % 60);
+    durationEl.textContent = formattedDuration;
+  });
+}
+
+function changeMusic(direction) {
+  musicIndex = (musicIndex + direction + songs.length) % songs.length;
+  loadMusic(songs[musicIndex]);
+  playMusic();
+}
+
+function updateProgressBar() {
+  const { duration, currentTime } = music;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+  const formattedCurrentTime =
+    formatTime(currentTime / 60) + ":" + formatTime(currentTime % 60);
+  currentTimeEl.textContent = formattedCurrentTime;
+}
+
+function setProgressBar(e) {
+  const width = playerProgress.clientWidth;
+  const clickX = e.offsetX;
+  music.currentTime = (clickX / width) * music.duration;
+}
+
+const formatTime = (time) => String(Math.floor(time)).padStart(2, "0");
+
+playBtn.addEventListener("click", togglePlay);
+prevBtn.addEventListener("click", () => changeMusic(-1));
+nextBtn.addEventListener("click", () => changeMusic(1));
+music.addEventListener("ended", () => changeMusic(1));
+music.addEventListener("timeupdate", updateProgressBar);
+playerProgress.addEventListener("click", setProgressBar);
+
+loadMusic(songs[musicIndex]);
